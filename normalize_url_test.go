c4507024c0310457da88ad262856520a1e9b7395
Johannes Ibald <johannes.ibald@etes.de>
2024-08-28 17:28:52 +0200

    additional testing for normalize_url

--- normalize_url_test.go
+++ normalize_url_test.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestNormalizeURL(t *testing.T) {
 	tests := []struct {
-		name     string
-		inputURL string
-		expected string
+		name          string
+		inputURL      string
+		expected      string
+		errorContains string
 	}{
 		{
 			name:     "remove scheme https",
@@ -32,18 +34,35 @@ func TestNormalizeURL(t *testing.T) {
 		},
 		{
 			name:     "capitals to lower",
-			inputURL: "https://BLOG.boot.dev/path/",
+			inputURL: "https://BLOG.boot.dev/PATH",
 			expected: "blog.boot.dev/path",
+		},
+		{
+			name:     "remove scheme, capitals and trailing slash",
+			inputURL: "https://BLOG.boot.dev/PATH/",
+			expected: "blog.boot.dev/path",
+		},
+		{
+			name:          "handle invalid URL",
+			inputURL:      `:\\invalidURL`,
+			expected:      "",
+			errorContains: "error parsing url",
 		},
 	}
 
 	for i, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			actual, err := normalizeURL(tc.inputURL)
-			if err != nil {
+			if err != nil && !strings.Contains(err.Error(), tc.errorContains) {
 				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
 				return
+			} else if err != nil && tc.errorContains == "" {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			} else if err == nil && tc.errorContains != "" {
+				t.Errorf("Test %v - '%s' FAIL: expected error containing '%v', but got none", i, tc.name, tc.errorContains)
 			}
+
 			if actual != tc.expected {
 				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
 			}
