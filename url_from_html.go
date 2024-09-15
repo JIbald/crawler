6497be7ee9505ee1ab05bb8079d00bd22f21a1e6
JIbald <j.ibald@protonmail.ch>
2024-09-15 19:04:25 +0200

    pass first test

--- url_from_html.go
+++ url_from_html.go
@@ -22,14 +22,26 @@ func getHrefValuesFromHTML(tree *html.Node) []string {
 	return result
 }
 
+func completeWithBaseURL(hrefValues []string, rawBaseURL string) []string {
+	result := []string{}
+	for _, val := range hrefValues {
+		if strings.Contains(val, "http") || strings.Contains(val, "https") || strings.Contains(val, rawBaseURL) {
+			result = append(result, val)
+		} else {
+			result = append(result, rawBaseURL+val)
+		}
+	}
+	return result
+}
+
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
 	result := []string{}
 	tree, err := html.Parse(strings.NewReader(htmlBody))
 	if err != nil {
 		return []string{}, fmt.Errorf("error Parsing ioReader: %w", err)
 	}
-	result = getHrefValuesFromHTML(tree)
-	fmt.Println(result)
+	hrefValues := getHrefValuesFromHTML(tree)
+	result = completeWithBaseURL(hrefValues, rawBaseURL)
 
 	return result, nil
 }
