31b58500a2a2200913e01e44630b8cbc82fe4bfc
Johannes Ibald <johannes.ibald@etes.de>
2024-09-27 16:11:28 +0200

    main prototype

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args
+	passedInArgs := args[1:]
+
+	if len(passedInArgs) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(passedInArgs) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	baseURL := passedInArgs[0]
+	fmt.Printf("starting crawl %v\n", baseURL)
 }
